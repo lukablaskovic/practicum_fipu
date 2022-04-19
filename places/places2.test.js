f0a9e01955a90054392c5e43dc2f2db48d2b9a05
lukablaskovic <lblaskovi@unipu.hr>
2022-04-19 21:44:33 +0200

    edit

--- places/places2.test.js
+++
@@ -1,7 +0,0 @@
-const getAllPlacesUnder = require("./places2");
-
-describe("Places", () => {
-  test("Ako je place = pula, vrati: veruda", () => {
-    expect(getAllPlacesUnder("pula")).toStrictEqual(["veruda"]);
-  });
-});

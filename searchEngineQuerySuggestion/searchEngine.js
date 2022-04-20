7ba19cea5aee2675c8569e711942f2e6d068dcf0
lukablaskovic <lblaskovi@unipu.hr>
2022-04-20 12:04:34 +0200

    searchEngine reduce

--- searchEngineQuerySuggestion/searchEngine.js
+++ searchEngineQuerySuggestion/searchEngine.js
@@ -1,4 +1,47 @@
-function findSuggestions() {
-  return 0;
+//"I have been, good. My best friend and I have fisherman and have fish."
+let phrase = [
+  "I",
+  "have",
+  "been",
+  ",",
+  "good",
+  ".",
+  "My",
+  "best",
+  "friend",
+  "and",
+  "I",
+  "have",
+  "fisherman",
+  "and",
+  "have",
+  "fish",
+  ".",
+];
+const stopWords = [".", ",", "!", "?", ";"];
+var indexGram = phrase.reduce(function (obj, word, currentIndex) {
+  if (!obj["stopwords"]) {
+    obj["stopwords"] = [];
+  }
+  if (!obj[word] && !stopWords.includes(word)) {
+    obj[word] = [currentIndex];
+  } else {
+    if (stopWords.includes(word)) {
+      obj["stopwords"].push(currentIndex);
+    } else obj[word].push(currentIndex);
+  }
+  return obj;
+}, {});
+console.log(indexGram);
+
+//findSuggestions("I have");
+
+function findSuggestions(searchTerm) {
+  const splits = searchTerm.split(" ");
+  console.log(splits);
+  //1. naći lokaciju I
+  //2. naći lokaciju have
+  //3. filtrirati samo kombinacije gdje su te dvije lokacije jedna za drugom
+  //4. pronaći riječi koje se nalaze na indexu have + 1
 }
 module.exports = findSuggestions;
--- searchEngineQuerySuggestion/searchEngine.test.js
+++ searchEngineQuerySuggestion/searchEngine.test.js
@@ -1,1 +1,9 @@
 const findSuggestions = require("./searchEngine");
+describe("searchEngine", () => {
+  test("Za 'I have' vrati sugestije: 'I have been' i 'I have fisherman'", () => {
+    expect(findSuggestions("I have")).toStrictEqual([
+      "I have been",
+      "I have fisherman",
+    ]);
+  });
+});
